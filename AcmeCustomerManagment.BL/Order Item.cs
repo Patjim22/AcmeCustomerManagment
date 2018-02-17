9b72bcaf8439d67c8a0a47e59e1c91746ab91cb7
Patjim202 <unconfigured@null.spigotmc.org>
2018-02-17 01:36:15 -0500

    Add project files.

---
+++ AcmeCustomerManagment.BL/Order Item.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcmeCustomerManagment.BL
+{
+    class Order_Item
+    {
+        public Order_Item()
+        {
+
+        }
+        public Order_Item(int orderItemId)
+        {
+            this.OrderItemId = orderItemId;
+        }
+
+        public int OrderItemId { get; private set; }
+        public int ProductID { get; set; }
+        public int OrderQuantity { get;  set; }
+        public decimal? PurchasePrice { get; set; }
+        
+        public bool Validate()
+        {
+            var isValid = true;
+
+            if (OrderItemId <= 0) isValid = false;
+            if (ProductID <= 0) isValid = false;
+            if (OrderQuantity <= 0) isValid = false;
+
+            return isValid;
+        }
+
+        public Order_Item Retrieve(int orderItemId)
+        {
+            return new Order_Item();
+        }
+
+        public bool Save()
+        {
+            return true;
+        }
+    }
+}
---
+++ CustomerTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CustomerTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CustomerTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6a8cb454-5414-4cfb-b56e-f4dc04209e96")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
